6cf3622008961c1ad06689026d58e10a6b536531
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-17 15:33:42 -0400

    deleted manifest files for a retry

--- src/sqllibMethods/Main.java
+++ src/sqllibMethods/Main.java
@@ -1,21 +1,29 @@
 package sqllibMethods;
 
-//import sqllibMethods.Interfaces.Readable;
+import sqllibMethods.Interfaces.Creatable;
+import sqllibMethods.Interfaces.Readable;
+// import sqllibMethods.Interfaces.Updatable;
+import sqllibMethods.Interfaces.Deletable;
 import java.util.LinkedList;
-import sqllibMethods.Secrets;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("error occurred");
+        }
+        // write your code here
         Secrets credentials = new Secrets();
 
         credentials.setUrl("jdbc:postgresql://localhost/test");
         credentials.setUsername("apinan");
         credentials.setPassword("admin");
 
-//        String[] columnsLabels = {"first_name", "last_name", "username", "friends", "companies"};
-//        print(Readable.readDB("users", columnsLabels, credentials));
+        String[] columnsLabels = {"first_name", "last_name", "username", "friends", "companies"};
+        print(Readable.readDB("users", columnsLabels, credentials));
     }
 
     public static void print(LinkedList<String[]> databaseData) {
